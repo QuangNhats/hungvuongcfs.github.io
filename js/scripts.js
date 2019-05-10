113bdb0a738f4302a35f5ec32727619237202131
QuangNhats <bbqntms@gmail.com>
2019-05-10 22:32:57 +0700

    New Upgrade

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,26 @@
 /*Copyright (c) 2019 Lavensoft, LEADER QUANG NHAT */
   $(document).ready(function(){
-    $('.parallax').parallax();
+    $('.parallax').parallax();//SETTING UP PARALLAX
+  });
+
+  $(document).on( 'scroll', function(){
+    var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+    var blur = 0;
+
+    /*///BLUR PARALLAX WHEN SCROLL
+    if(currentScroll > 0){
+        blur = currentScroll - 65;
+    }else if(blur < 0){
+        blur = 0;
+    }
+    if(blur > 100){
+        blur = 100;
+    }
+    $(".parallax img").css({"filter" : "blur("+blur+"px)"});
+    */
+    //NAV BAR EFFECT SCROLL
+    $("#nav-bar").addClass("nav-toggle");
+    if (currentScroll == 0) {
+        $("#nav-bar").removeClass("nav-toggle");
+    }
   });
